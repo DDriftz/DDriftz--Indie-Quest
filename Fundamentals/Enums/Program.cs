d05c76c93952b654affdfbe452d1d9a5c122bf07
DDriftz <Hussein86.jobb@gmail.com>
2025-05-29 14:44:12 +0200

    DDriftz Indie Quest

--- Fundamentals/Enums/Program.cs
+++ Fundamentals/Enums/Program.cs
@@ -1,147 +1,97 @@
+﻿﻿
 ﻿using System;
-using System.Text;
+using System.ComponentModel.DataAnnotations;
+using static enums.TheGame;
 
-enum Suit
+namespace enums
 {
-    Heart,
-    Spade,
-    Diamond,
-    Club
-}
-
-class CardDrawer
-{
-    static void Main()
+    internal class Program
     {
-        Console.OutputEncoding = Encoding.UTF8;
-
-        // Test DrawAce with each suit
-        DrawAce(Suit.Spade);
-        Console.WriteLine();
-        DrawAce(Suit.Heart);
-        Console.WriteLine();
-
-        // Draw full suit from Ace (1) to King (13)
-        for (int rank = 1; rank <= 13; rank++)
+        static void Main(string[] args)
         {
-            DrawCard(Suit.Spade, rank);
-            Console.WriteLine();
+          TheGame theGame = new TheGame();
+            theGame.DrawAce(Suits.Spades);
+
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 13; j++)
+                {
+                    theGame.DrawCard((Suits)i, j);
+                }
+            }
+
         }
     }
-
-    static void DrawAce(Suit suit)
+    public class TheGame
     {
-        char symbol = GetSuitSymbol(suit);
-
-        Console.WriteLine("╭───────╮");
-        Console.WriteLine($"│A      │");
-        Console.WriteLine($"│{symbol}      │");
-        Console.WriteLine($"│   {symbol}   │");
-        Console.WriteLine($"│      {symbol}│");
-        Console.WriteLine($"│      A│");
-        Console.WriteLine("╰───────╯");
-    }
-
-    static void DrawCard(Suit suit, int rank)
-    {
-        string rankStr = GetRankLabel(rank);
-        char symbol = GetSuitSymbol(suit);
-
-        string[] template = new string[9];
-        template[0] = "╭─────────╮";
-
-        if (rank == 1) // Ace
+        public void DrawAce(Suits suit)
         {
-            template[1] = $"│{rankStr,-2}       │";
-            template[2] = $"│{symbol}         │";
-            template[3] = $"│         │";
-            template[4] = $"│    {symbol}    │";
-            template[5] = $"│         │";
-            template[6] = $"│        {symbol}│";
-            template[7] = $"│       {rankStr,2}│";
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            switch (suit)
+            {
+                case Suits.Hearts:
+                    Console.WriteLine("╭───────╮\r\n│A      │\r\n│♥      │\r\n│   ♥   │\r\n│      ♥│\r\n│      A│\r\n╰───────╯\r\n");
+                    break;
+                case Suits.Clubs:
+                    Console.WriteLine("╭───────╮\r\n│A      │\r\n│♣      │\r\n│   ♣   │\r\n│      ♣│\r\n│      A│\r\n╰───────╯\r\n");
+                    break;
+                case Suits.Spades:
+                    Console.WriteLine("╭───────╮\r\n│A      │\r\n│♠      │\r\n│   ♠   │\r\n│      ♠│\r\n│      A│\r\n╰───────╯\r\n");
+                    break;
+                case Suits.Diamonds:
+                    Console.WriteLine("╭───────╮\r\n│A      │\r\n│♦      │\r\n│   ♦   │\r\n│      ♦│\r\n│      A│\r\n╰───────╯\r\n");
+                    break;
+            }
         }
-        else if (rank >= 2 && rank <= 10)
+        public void DrawCard(Suits suit, int rank)
         {
-            // Create symbol positions based on rank (basic symmetrical layout)
-            var grid = new string[5];
-            for (int i = 0; i < grid.Length; i++) grid[i] = "         ";
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
 
-            // Place symbols for numeric ranks (simplified symmetrical layout)
-            List<(int, int)> positions = GetSymbolPositions(rank);
-            char[,] matrix = new char[5, 9];
-            for (int r = 0; r < 5; r++)
-                for (int c = 0; c < 9; c++)
-                    matrix[r, c] = ' ';
+            String[] cardNumbers = {
+               "╭─────────╮\r\n│A        │\r\n│♠        │\r\n│         │\r\n│    ♠    │\r\n│         │\r\n│        ♠│\r\n│        A│\r\n╰─────────╯\r\n",
+               "╭─────────╮\r\n│2   ♠    │\r\n│♠        │\r\n│         │\r\n│         │\r\n│         │\r\n│        ♠│\r\n│    ♠   2│\r\n╰─────────╯\r\n",
+               "╭─────────╮\r\n│3   ♠    │\r\n│♠        │\r\n│         │\r\n│    ♠    │\r\n│         │\r\n│        ♠│\r\n│    ♠   3│\r\n╰─────────╯\r\n",
+               "╭─────────╮\r\n│4 ♠   ♠  │\r\n│♠        │\r\n│         │\r\n│         │\r\n│         │\r\n│        ♠│\r\n│  ♠   ♠ 4│\r\n╰─────────╯\r\n",
+               "╭─────────╮\r\n│5 ♠   ♠  │\r\n│♠        │\r\n│         │\r\n│    ♠    │\r\n│         │\r\n│        ♠│\r\n│  ♠   ♠ 5│\r\n╰─────────╯\r\n",
+               "╭─────────╮\r\n│6 ♠   ♠  │\r\n│♠        │\r\n│         │\r\n│  ♠   ♠  │\r\n│         │\r\n│        ♠│\r\n│  ♠   ♠ 6│\r\n╰─────────╯\r\n",
+               "╭─────────╮\r\n│7 ♠   ♠  │\r\n│♠        │\r\n│    ♠    │\r\n│  ♠   ♠  │\r\n│         │\r\n│        ♠│\r\n│  ♠   ♠ 7│\r\n╰─────────╯\r\n",
+               "╭─────────╮\r\n│8 ♠   ♠  │\r\n│♠        │\r\n│    ♠    │\r\n│  ♠   ♠  │\r\n│    ♠    │\r\n│        ♠│\r\n│  ♠   ♠ 8│\r\n╰─────────╯\r\n",
+               "╭─────────╮\r\n│9 ♠   ♠  │\r\n│♠        │\r\n│  ♠   ♠  │\r\n│    ♠    │\r\n│  ♠   ♠  │\r\n│        ♠│\r\n│  ♠   ♠ 9│\r\n╰─────────╯\r\n",
+               "╭─────────╮\r\n│10♠   ♠  │\r\n│♠   ♠    │\r\n│  ♠   ♠  │\r\n│         │\r\n│  ♠   ♠  │\r\n│    ♠   ♠│\r\n│  ♠   ♠10│\r\n╰─────────╯\r\n",
+               "╭─────────╮\r\n│J┌─────┐ │\r\n│♠│♠\\__/│ │\r\n│ │|(_/|│ │\r\n│ │} / {│ │\r\n│ │|/_)|│ │\r\n│ │/  \\♠│♠│\r\n│ └─────┘J│\r\n╰─────────╯\r\n",
+               "╭─────────╮\r\n│Q┌─────┐ │\r\n│♠│♠(_(/│ │\r\n│ │  )/❀│ │\r\n│ │{ / }│ │\r\n│ │❀/(  │ │\r\n│ │/) )♠│♠│\r\n│ └─────┘Q│\r\n╰─────────╯\r\n",
+               "╭─────────╮\r\n│K┌─────┐ │\r\n│♠│♠\\__/│ │\r\n│ │ (_/|│ │\r\n│ │+ / +│ │\r\n│ │|/_) │ │\r\n│ │/  \\♠│♠│\r\n│ └─────┘K│\r\n╰─────────╯\r\n"
+            };
 
-            foreach (var (r, c) in positions)
-                if (r >= 0 && r < 5 && c >= 0 && c < 9)
-                    matrix[r, c] = symbol;
-
-            template[1] = $"│{rankStr,-2}{matrix[0, 4]}   {matrix[0, 8]}│";
-            template[2] = $"│{matrix[1, 0]}         │";
-            template[3] = $"│  {matrix[2, 3]}   {matrix[2, 5]}  │";
-            template[4] = $"│    {matrix[3, 4]}    │";
-            template[5] = $"│         {matrix[4, 8]}│";
-            template[6] = $"│  {matrix[4, 3]}   {matrix[4, 5]} {rankStr,2}│";
-            template[7] = $"│         │";
+            // replacing the suit icon in the string
+            Console.WriteLine(cardNumbers[rank].Replace("♠", getStringACE(suit)));
         }
-        else // J, Q, K
+        
+        private string getStringACE(Suits suit)
         {
-            string face = rankStr;
-            template[1] = $"│{face}┌─────┐ │";
-            template[2] = $"│{symbol}│{symbol}\\__/│ │";
-            template[3] = $"│ │|(_/|│ │";
-            template[4] = $"│ │}} / {{│ │";
-            template[5] = $"│ │|/_)|│ │";
-            template[6] = $"│ │/  \\{symbol}│{symbol}│";
-            template[7] = $"│ └─────┘{face}│";
+            switch (suit)
+            {
+                case Suits.Hearts:
+                    return "♥";
+                case Suits.Clubs:
+                    return "♣";
+                case Suits.Spades:
+                    return "♠";
+                case Suits.Diamonds:
+                    return "♦";
+                default:
+                    Console.WriteLine("ERROR! Dumbass typed wrong number");
+                    return "ERROR!";
+            }
         }
 
-        template[8] = "╰─────────╯";
-
-        foreach (var line in template)
-            Console.WriteLine(line);
-    }
-
-    static string GetRankLabel(int rank)
+        public enum Suits
     {
-        return rank switch
-        {
-            1 => "A",
-            11 => "J",
-            12 => "Q",
-            13 => "K",
-            _ => rank.ToString()
-        };
+        Hearts = 0,
+        Clubs = 1,
+        Spades = 2,
+        Diamonds = 3,
     }
-
-    static char GetSuitSymbol(Suit suit)
-    {
-        return suit switch
-        {
-            Suit.Heart => '♥',
-            Suit.Spade => '♠',
-            Suit.Diamond => '♦',
-            Suit.Club => '♣',
-            _ => '?'
-        };
-    }
-
-    static List<(int, int)> GetSymbolPositions(int rank)
-    {
-        var positions = new List<(int, int)>();
-        switch (rank)
-        {
-            case 2: positions.AddRange([(1, 0), (4, 8)]); break;
-            case 3: positions.AddRange([(1, 0), (2, 4), (4, 8)]); break;
-            case 4: positions.AddRange([(1, 0), (1, 8), (4, 0), (4, 8)]); break;
-            case 5: positions.AddRange([(1, 0), (1, 8), (2, 4), (4, 0), (4, 8)]); break;
-            case 6: positions.AddRange([(1, 0), (1, 8), (3, 0), (3, 8), (4, 0), (4, 8)]); break;
-            case 7: positions.AddRange([(1, 0), (1, 8), (2, 4), (3, 0), (3, 8), (4, 0), (4, 8)]); break;
-            case 8: positions.AddRange([(1, 0), (1, 8), (2, 4), (3, 0), (3, 8), (4, 0), (4, 8), (0, 4)]); break;
-            case 9: positions.AddRange([(0, 4), (1, 0), (1, 8), (2, 2), (2, 6), (3, 0), (3, 8), (4, 2), (4, 6)]); break;
-            case 10: positions.AddRange([(0, 1), (0, 4), (0, 7), (1, 0), (1, 8), (3, 1), (3, 4), (3, 7), (4, 0), (4, 8)]); break;
-        }
-        return positions;
     }
 }
--- Fundamentals/Enums/obj/Debug/net9.0/Enums.AssemblyInfo.cs
+++ Fundamentals/Enums/obj/Debug/net9.0/Enums.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Enums")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e1e26dd04f0997c77482e53147b75c451d16078")]
 [assembly: System.Reflection.AssemblyProductAttribute("Enums")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Enums")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
